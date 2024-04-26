faa98090dd33a0bde4bac2a96537bd07e1987ebf
doko972 david <doko972@gmail.com>
2024-04-26 11:59:24 +0200

    text 'Avantages' and 'Caractéristiques' hidden, and show on click

--- js/script.js
+++ js/script.js
@@ -33,34 +33,57 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 
-});
-//////////
-/*PANIER*/
-/////////
-// Au clic sur le bouton d’ajout au panier, le compteur du panier présent dans le header se
-// met à jour avec la quantité de produit ajoutée via le champ à gauche du bouton.
-// Si la quantité de produits au panier est supérieure à 99 le compteur affiche “99+”.
-// Une fois le produit ajouté au panier, le bouton d’ajout est désactivé :
-// ● le style du bouton est actualisé pour indiquer le changement d’état ;
-// ● le texte du bouton change pour “Déjà au panier” ;
-// ● il n’est plus possible de cliquer sur le bouton et d’ajouter le produit au panier.
-const addBtn = document.querySelector(".add-cta");
-const addQty = document.querySelector(".add-qty");//qtyInput
-const cartNb = document.querySelector(".cart-nb");//cartCounter
 
-/**
- * update "panier" counter with text content if quantity > 99
- * @param {number} quantity quantity
- */
-function updateQuantity(quantity) {
-    if (quantity > 99) {
-        cartNb.textContent = "99+";
-    } else {
-        cartNb.textContent = quantity;
+
+
+
+    //////////
+    /*PANIER*/
+    /////////
+    // Au clic sur le bouton d’ajout au panier, le compteur du panier présent dans le header se
+    // met à jour avec la quantité de produit ajoutée via le champ à gauche du bouton.
+    // Si la quantité de produits au panier est supérieure à 99 le compteur affiche “99+”.
+    // Une fois le produit ajouté au panier, le bouton d’ajout est désactivé :
+    // ● le style du bouton est actualisé pour indiquer le changement d’état ;
+
+
+
+
+    const addBtn = document.querySelector(".add-cta");
+    const addQty = document.querySelector(".add-qty");
+    const cartNb = document.querySelector(".cart-nb");
+
+    /**
+     * update "panier" counter with text content if quantity > 99
+     * @param {number} quantity quantity
+     */
+    function updateQuantity(quantity) {
+        if (quantity > 99) {
+            cartNb.textContent = "99+";
+        } else {
+            cartNb.textContent = quantity;
+        }
     }
-}
 
+    function addToCard() {
+        const quantity = parseInt(addQty.value);
+        updateQuantity(quantity);
+        // ● il n’est plus possible de cliquer sur le bouton et d’ajouter le produit au panier.
+        addBtn.disable = true;
+        // ● le texte du bouton change pour “Déjà au panier” ;
+        addBtn.textContent = 'Déjà au panier';
 
-addBtn.addEventListener('click', function () {
+    }
+    addBtn.addEventListener('click', function () {
+        addToCard();
+        console.log('click panier');
+    });
 
-})
+});
+//Hidde text//
+document.querySelector("#product-subttl").addEventListener('click', function (event) {
+    document.querySelector("#text1").classList.toggle('text-hidden');
+});
+document.querySelector("#product-subttl-sd").addEventListener('click', function (event) {
+    document.querySelector("#text2").classList.toggle('text-hidden');
+});
