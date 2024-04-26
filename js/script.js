136e0a16c03cbd7f79f66d55a7e6cb8b4c7006f5
doko972 david <doko972@gmail.com>
2024-04-26 16:55:04 +0200

    Final correction - is not finish

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 
 document.addEventListener("DOMContentLoaded", function () {
-
     const picturesImg = document.querySelector(".pictures-img");
     const thumbsImgs = document.querySelectorAll(".thumbs-img");
     const prevBtn = document.querySelector(".pictures-prev");
@@ -9,16 +8,25 @@ document.addEventListener("DOMContentLoaded", function () {
     const imagePaths = Array.from(thumbsImgs).map(img => img.src.replace("-s.png", "-l.png"));
 
     let currentIndex = 0;
-
+    /**
+     * Get img with index
+     * @param {number} index index number
+     */
     function showImage(index) {
         picturesImg.src = imagePaths[index];
     }
-
+    /**
+     * Change Index of img in +
+     * @param {number} index index number
+     */
     function showNextImage() {
         currentIndex = (currentIndex + 1) % imagePaths.length;
         showImage(currentIndex);
     }
-
+    /**
+     * Change Index of img in -
+     * @param {number} index index number
+     */
     function showPrevImage() {
         currentIndex = (currentIndex - 1 + imagePaths.length) % imagePaths.length;
         showImage(currentIndex);
@@ -31,82 +39,59 @@ document.addEventListener("DOMContentLoaded", function () {
             currentIndex = index;
             showImage(currentIndex);
         });
-    });
 
-    const addBtn = document.querySelector(".add-cta");
-    const addQty = document.querySelector(".add-qty");
-    const cartNb = document.querySelector(".cart-nb");
 
-    /**
-     * update "panier" counter with text content if quantity > 99
-     * @param {number} quantity quantity
-     */
-    function updateQuantity(quantity) {
-        if (quantity > 99) {
-            cartNb.textContent = "99+";
-        } else {
-            cartNb.textContent = quantity;
+        const addBtn = document.querySelector(".add-cta");
+        const addQty = document.querySelector(".add-qty");
+        const cartNb = document.querySelector(".cart-nb");
+
+        /**
+         * update "panier" counter with text content if quantity > 99
+         * @param {number} quantity quantity
+         */
+        function updateQuantity(quantity) {
+            if (quantity > 99) {
+                cartNb.textContent = "99+";
+            } else {
+                cartNb.textContent = quantity;
+            }
         }
-    }
 
-    function addToCard() {
-        const quantity = parseInt(addQty.value);
-        updateQuantity(quantity);
-        addBtn.disable = true;
-        addBtn.textContent = 'Déjà au panier';
+        function addToCard() {
+            const quantity = parseInt(addQty.value);
+            updateQuantity(quantity);
+            addBtn.disable = true;
+            addBtn.textContent = 'Déjà au panier';
 
-    }
-    addBtn.addEventListener('click', function () {
-        addToCard();
-        console.log('click panier');
+        }
+        addBtn.addEventListener('click', function () {
+            addToCard();
+            console.log('click panier');
+        });
+
+    });
+    /**Hidden text */
+    document.querySelector("#product-subttl").addEventListener('click', function () {
+        document.querySelector("#text1").classList.toggle('text-hidden');
+    });
+    document.querySelector("#product-subttl-sd").addEventListener('click', function () {
+        document.querySelector("#text2").classList.toggle('text-hidden');
     });
 
+
+
+    /*IMG in bottom page*/
+    const similarLstImg = document.querySelector(".similar-itm");
+    const prevBtnLeft = document.querySelector(".pictures-left");
+    const nextBtnRigth = document.querySelector(".pictures-rigth");
+
+    // function showNextImage() {
+    //     similarLstImg = (similarLstImg + 1) % similarLstImg.length;
+    // }
+    // function showPrevImage() {
+    //     similarLstImg = (similarLstImg - 1 + similarLstImg.length) % similarLstImg.length;
+    // }
+    prevBtnLeft.addEventListener("click", showPrevImage);
+    nextBtnRigth.addEventListener("click", showNextImage);
 });
-/**Hidden text */
-document.querySelector("#product-subttl").addEventListener('click', function (event) {
-    document.querySelector("#text1").classList.toggle('text-hidden');
-});
-document.querySelector("#product-subttl-sd").addEventListener('click', function (event) {
-    document.querySelector("#text2").classList.toggle('text-hidden');
-});
-
-
-/*IMG in bottom page*/
-const leftBtn = document.querySelector(".pictures-left");
-const rigthBtn = document.querySelector(".pictures-rigth");
-
-
-function init() {
-    ul = document.getElementById('similarLst');
-    liItems = ul.children;
-    imageNumber = liItems.length;
-    imageWidth = liItems[0].children[0].clientWidth;
-
-
-    
-    const imagePaths = Array.from(thumbsImgs).map(img => img.src.replace(".png"));
-
-    let currentIndex = 0;
-
-    function showImage(index) {
-        picturesImg.src = imagePaths[index];
-    }
-
-    function showNextImage() {
-        currentIndex = (currentIndex + 1) % imagePaths.length;
-        showImage(currentIndex);
-    }
-
-    function showPrevImage() {
-        currentIndex = (currentIndex - 1 + imagePaths.length) % imagePaths.length;
-        showImage(currentIndex);
-    }
-
-    leftBtn.onclick = function () { 
-        showNextImage(); 
-    };
-    rigthBtn.onclick = function () { 
-        showPrevImage(); 
-    };
-}
-
+/////////////////////////////////////////
